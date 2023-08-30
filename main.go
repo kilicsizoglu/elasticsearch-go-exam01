7c8b0cb7b067e8d2579d11bc1d43eeaa06785e70
Fatih YOLCU <kilicsizoglu@gmail.com>
2023-08-30 21:23:04 +0300

    init

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/elastic/go-elasticsearch/v8"
+	"io"
+	"log"
+)
+
+func main() {
+
+	es, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+	log.Println(elasticsearch.Version)
+
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(res.Body)
+
+	log.Println(res)
+
+}
